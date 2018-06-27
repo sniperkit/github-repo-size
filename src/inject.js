5c612878ebf391f185cc51aec83059bf2735f787
Rosco Pecoltran <rosco_pecoltran@msn.com>
2018-06-27 12:59:03 +0200

    colorize size by range, exclude uris from get-repo-size

--- src/inject.js
+++ src/inject.js
@@ -4,13 +4,16 @@ const API = 'https://api.github.com/repos/'
 const LI_TAG_ID = 'github-repo-size'
 const GITHUB_TOKEN_KEY = 'x-github-token'
 
-const storage = chrome.storage.sync || chrome.storage.local
+var excludeURIs = [
+  "github.com/nodejs/node",
+  "github.com/tensorflow/tensorflow",
+  "github.com/django/django",
+]
 
 let githubToken
 
 const isTree = (uri) => {
   const repoURI = uri.split('/')
-
   return repoURI.length === 2 || repoURI[2] === 'tree'
 }
 
@@ -31,21 +34,44 @@ const getRepoContentURI = (uri) => {
   return repoURI.join('/')
 }
 
+const getRepoTreeURI = (uri) => {
+    const repoURI = uri.split('/')
+    const treeBranch = repoURI.splice(2)
+
+    repoURI.push('git/trees')
+    if (treeBranch && treeBranch[1]) {
+        repoURI.push(treeBranch[1])
+    } else {
+        repoURI.push('master')
+    }
+
+    return repoURI.join('/') + '?recursive=1'
+}
+
 const getHumanReadableSizeObject = (bytes) => {
   if (bytes === 0) {
     return {
       size: 0,
-      measure: 'Bytes'
+      measure: 'Bytes',
+      style: ''
     }
   }
 
   const K = 1024
-  const MEASURE = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  const MEASURE = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
   const i = Math.floor(Math.log(bytes) / Math.log(K))
+
+  var colorStyle = '#6a737d'
+  if (i == 2) {
+    colorStyle = 'orange'
+  } else if (i > 2) {
+    colorStyle = 'red'
+  }
 
   return {
     size: parseFloat((bytes / Math.pow(K, i)).toFixed(2)),
-    measure: MEASURE[i]
+    measure: MEASURE[i],
+    style: colorStyle,
   }
 }
 
@@ -54,13 +80,14 @@ const getHumanReadableSize = (size) => {
 
   const t = getHumanReadableSizeObject(size)
 
-  return t.size + ' ' + t.measure
+  return '<span style="color:'+t.style+'!important;">' + t.size + ' ' + t.measure + '</span>'
 }
 
 const getSizeHTML = (size) => {
   const humanReadableSize = getHumanReadableSizeObject(size)
 
   return '<li id="' + LI_TAG_ID + '">' +
+    '<a>' +
     '<svg class="octicon octicon-database" aria-hidden="true" height="16" version="1.1" viewBox="0 0 12 16" width="12">' +
     '<path d="M6 15c-3.31 0-6-.9-6-2v-2c0-.17.09-.34.21-.5.67.86 3 1.5 5.79 1.5s5.12-.64 5.79-1.5c.13.16.21.33.21.5v2c0 1.1-2.69 2-6 2zm0-4c-3.31 0-6-.9-6-2V7c0-.11.04-.21.09-.31.03-.06.07-.13.12-.19C.88 7.36 3.21 8 6 8s5.12-.64 5.79-1.5c.05.06.09.13.12.19.05.1.09.21.09.31v2c0 1.1-2.69 2-6 2zm0-4c-3.31 0-6-.9-6-2V3c0-1.1 2.69-2 6-2s6 .9 6 2v2c0 1.1-2.69 2-6 2zm0-5c-2.21 0-4 .45-4 1s1.79 1 4 1 4-.45 4-1-1.79-1-4-1z"></path>' +
     '</svg>' +
@@ -68,6 +95,7 @@ const getSizeHTML = (size) => {
     humanReadableSize.size +
     '</span> ' +
     humanReadableSize.measure +
+    '</a>' +
     '</li>'
 }
 
@@ -89,7 +117,17 @@ const parseJSON = (response) => {
 
 const getAPIData = (uri, callback) => {
   const headerObj = {
-    'User-Agent': 'harshjv/github-repo-size'
+    'User-Agent': 'sniperkit/snk.chrome.github-repo-size'
+  }
+
+  console.log("current uri: " + uri )
+  console.log("exclude uri: " + excludeURIs.includes(uri) )
+
+  const exclude = excludeURIs.includes(uri)
+
+  if (exclude) {
+    console.log("skipping uri from repo-size calculation: " + uri )
+    return
   }
 
   const token = localStorage.getItem(GITHUB_TOKEN_KEY) || githubToken
@@ -112,8 +150,8 @@ const getAPIData = (uri, callback) => {
 const getFileName = (text) => text.trim().split('/')[0]
 
 const checkForRepoPage = () => {
-  let repoURI = window.location.pathname.substring(1)
-  repoURI = repoURI.endsWith('/') ? repoURI.slice(0, -1) : repoURI
+  const repoURI = window.location.pathname.substring(1)
+  const repoPath = repoURI.split('/').splice(4).join('/').trim()
 
   if (isTree(repoURI)) {
     const ns = document.querySelector('ul.numbers-summary')
@@ -129,11 +167,14 @@ const checkForRepoPage = () => {
     }
 
     if (!tdElems) {
-      getAPIData(getRepoContentURI(repoURI), (data) => {
+      getAPIData(getRepoTreeURI(repoURI), (data) => {
         const sizeArray = {}
 
-        for (const item of data) {
-          sizeArray[item.name] = item.type !== 'dir' ? item.size : null
+        for (const item of data.tree) {
+          if (item.path.startsWith(repoPath)) {
+            const commonPathPrefix = item.path.replace(new RegExp('^' + repoPath + '/?'), '').split('/')[0]
+            sizeArray[commonPathPrefix] = (sizeArray[commonPathPrefix] || 0) + (item.size || 0)
+          }
         }
 
         const list = document.querySelectorAll('table > tbody tr.js-navigation-item:not(.up-tree)')
@@ -156,7 +197,7 @@ const checkForRepoPage = () => {
   }
 }
 
-storage.get(GITHUB_TOKEN_KEY, function (data) {
+chrome.storage.sync.get(GITHUB_TOKEN_KEY, (data) => {
   githubToken = data[GITHUB_TOKEN_KEY]
 
   chrome.storage.onChanged.addListener((changes, namespace) => {
